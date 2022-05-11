b7f3df50952f8f58830fa54bac8175cbcd795f06
xankman <xankman@gmail.com>
2022-05-11 11:44:22 -0500

    Change the code to return user = elonmusk tweets and created columns User and Tweet.

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import pandas as pd
 from auth import connect_to_twitter_oauth
-"""
-musk_tweets = api.user_timeline('elonmusk')
-
-for tweet in musk_tweets:
-    print(tweet.text)
-
-"""
 
 api = connect_to_twitter_oauth()
-public_tweets = api.home_timeline()
+musk_tweets = api.user_timeline(screen_name='elonmusk')
 
-print(public_tweets[0].text)
-print(public_tweets[0].user.screen_name)
-
-columns = ['Time', 'User', 'Tweet']
+columns = ['User', 'Tweet']
 data = []
-for tweet in public_tweets:
-    data.append([tweet.created_at, tweet.user.screen_name, tweet.text])
+
+for tweet in musk_tweets:
+    data.append([tweet.user.screen_name, tweet.text])
+
+df = pd.DataFrame(data, columns=columns)
+
+print(df)
 
 df = pd.DataFrame(data, columns=columns)
 
@@ -29,4 +24,3 @@ print(df)
 # Create Read Update Delete MySQL database and connect to database.
 #    mysql_create.py - create database and table(s)
 #    mysql_update.py - write data to the table(s)
-"""
