e7fc3f413e151d70608c406e84d9b060a738c4ac
xankman <xankman@gmail.com>
2022-05-12 20:04:44 -0500

    installed mysql-connector

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pandas as pd
 from auth import connect_to_twitter_oauth
 
+
+# connect to twitter api
 api = connect_to_twitter_oauth()
 musk_tweets = api.user_timeline(screen_name='elonmusk')
+
 
 columns = ['User', 'Tweet']
 data = []
 
+# iterate through the tweets and add to list
 for tweet in musk_tweets:
     data.append([tweet.user.screen_name, tweet.text])
 
+# add this -> public_metrics to get tweet impressions
+
+# create a data frame of the data, ie. transform the data to column format
 df = pd.DataFrame(data, columns=columns)
 
 print(df)
 
-#df.to_csv('tweets.csv')
+# df.to_csv('tweets.csv')
 
 
 # TODO
--- mysql_other.py
+++ mysql_other.py
@@ -5,4 +5,7 @@ def connect_to_database():
  #   connector = WHAT EVER CONNECTOR FUNCTION I CAN USE TO PASS THE DB PASSWORD
 """
 
+#mysql_pass = 'cryptomake'
+
 # TODO
+# install mysql-connector-python using pip through pycharm terminal
