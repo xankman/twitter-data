8573ef08daf3ce5cc8de50d4ae8b06664457c9bb
xankman <xankman@gmail.com>
2022-05-23 09:06:34 -0500

    Made edits

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This project uses the Twitter API to extract data from Twitter public tweets of a Twitter user.
 
-It returns the tweet and screen name right now. More to come.
+It returns the tweet and screen name then using a DataFrame from pandas to columnize the data and then connects to a MySQL db and writes to a table.
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 
 pymysql.install_as_MySQLdb()
 
-# Connect to Twitter API
+# Connect to Twitter API. Pick a user. I used Elon but it can be any Twitter user
 api = connect_to_twitter_oauth()
 musk_tweets = api.user_timeline(screen_name='elonmusk')
 
@@ -22,18 +22,16 @@ df = pd.DataFrame(data, columns=columns)
 # print(df)
 
 print("\nConnecting to MySQL...\n")
-# Create engine. Need to work on using config.ini for password so no hardcoding here.
-engine = create_engine('mysql://root:cryptomake@localhost/tweetdb')
+# Create engine. Replace with your own input where applicable
+engine = create_engine('mysql://<INSERT USERNAME>:<INSERT DB PASSWORD>@<INSERT HOSTNAME>/<INSERT DB NAME>')
 print("Connected.\n")
 print("Writing Data...")
 
 # Create the connection and close it (whether successfully connected or failed).
 with engine.begin() as connection:
-    df.to_sql(name='tweets_table', con=connection, if_exists='replace', index=False)
+    df.to_sql(name='<INSERT TABLE NAME>', con=connection, if_exists='replace', index=False)
 
 print("Data written. Now closing connection...")
 
+# You can also write to a CSV file with the following function
 # df.to_csv('tweets.csv')
-
-# TODO
-# Need to check if data already in table. Append only new data.
